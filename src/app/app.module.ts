567067035906eca5e14dc7f204d495568ac79487
Salvatore McCarty <salvatore.mccarty@gmail.com>
2017-05-22 00:09:08 +0000

    pausing our project,we got the user info to load & display

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HttpModule } from '@angular/http';
+
+import { OrganizationsPage } from '../pages/organizations/organizations';
+import { UsersPage } from '../pages/users/users';
+import { ReposPage } from '../pages/repos/repos';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { GithubUsersProvider } from '../providers/github-users/github-users';
+
+let injectables = [
+    MyApp,
+    UsersPage,
+    ReposPage,
+    OrganizationsPage
+  ];
+
+@NgModule({
+  declarations: injectables,
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    HttpModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: injectables,
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    GithubUsersProvider
+  ]
+})
+export class AppModule {}
---
+++ src/providers/github-users/github-users.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+
+import { User } from '../../models/user';
+/*
+  Generated class for the GithubUsersProvider provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class GithubUsersProvider {
+  githubApiUrl = 'https://api.github.com';
+
+  constructor(public http: Http) {
+    console.log('Hello GithubUsersProvider Provider');
+  }
+  
+  
+  //this is where we're going to add our method
+  load(): Observable<User[]> {
+    return this.http.get(this.githubApiUrl + '/users')
+    .map(res => <User[]>res.json());
+  }
+}
